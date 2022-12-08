02b4e479595dd958dc093d9f1508d24b424bab65
Luca Mock <luca.mock@ringieraxelspringer.ch>
2022-12-08 22:04:14 +0100

    created navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css';
 
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
 import React from 'react';
 
 const App = (): JSX.Element => {
   return (
     <div className="App">
-      <h1>Morning</h1>
+      Header
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>Main stuff</div>} />
+        </Routes>
+      </Router>
+      Footer
     </div>
   );
 };

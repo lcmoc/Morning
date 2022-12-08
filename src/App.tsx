6a5f7dfccd0ba03ad6f7ab77ea34b77afc7644d8
Luca Mock <luca.mock@ringieraxelspringer.ch>
2022-12-08 22:47:50 +0100

    creadted header template

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import './App.css';
 
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
+import Footer from './components/Footer';
+import Header from './components/Header';
 import React from 'react';
 
 const App = (): JSX.Element => {
   return (
     <div className="App">
-      Header
+      <Header />
       <Router>
         <Routes>
           <Route path="/" element={<div>Main stuff</div>} />
         </Routes>
       </Router>
-      Footer
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+const Footer = (): JSX.Element => <div>Footer</div>;
+
+export default Footer;

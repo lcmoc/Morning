8ceb7ea043b0de740e2960c658479c92e436b693
lcmoc <69789995+lcmoc@users.noreply.github.com>
2022-12-16 07:50:03 +0100

    Daydiagram & Start of Weekdiagram (#1)
    * dayly commit
    
    * finished day diagram

---
+++ src/components/Diagrams/DayDiagram/index.tsx
@@ -0,0 +1,64 @@
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LineElement,
+  LinearScale,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js';
+
+import { Line } from 'react-chartjs-2';
+import React from 'react';
+
+interface WeekDiagramProps {
+  times: String[];
+  measures: number[];
+}
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Wetterdaten',
+    },
+  },
+};
+
+const DayDiagram = ({ times, measures }: WeekDiagramProps): JSX.Element => {
+  const data = {
+    labels: times,
+    datasets: [
+      {
+        label: 'Grad',
+        data: measures,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <Line options={options} data={data} />
+    </>
+  );
+};
+
+export default DayDiagram;
---
+++ src/screens/Weather/index.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import { getMeasures, getTimes } from '../../components/Helpers';
+
+import CurrentTemperatureCard from '../../components/CurrentTemperatureCard';
+import DayDiagram from '../../components/Diagrams/DayDiagram';
+import { Grid } from '@mui/material';
+
+interface ApiData {
+  time: [];
+}
+
+const Weather = (): JSX.Element => {
+  const [apiData, setApiData] = useState<any | null>(null);
+
+  async function getDataFromAPI(): Promise<ApiData | undefined> {
+    try {
+      const response = await fetch(
+        'https://api.open-meteo.com/v1/forecast?latitude=47.37&longitude=8.55&hourly=temperature_2m&current_weather=true&timezone=Europe%2FBerlin&past_days=1',
+      );
+
+      return await response.json();
+    } catch (error) {
+      console.error(error); //eslint-disable-line
+    }
+  }
+
+  useEffect(() => {
+    getDataFromAPI()
+      .then((data) => setApiData(data))
+      .catch((error) => {
+        console.error(error); //eslint-disable-line
+      });
+  }, []);
+  if (apiData === null) {
+    return <div>Loading...</div>;
+  }
+
+  const currentTemp = apiData?.current_weather;
+
+  return (
+    <div>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="space-around"
+      >
+        <h1>Weather</h1>
+        <CurrentTemperatureCard
+          temperature={currentTemp.temperature}
+          time={currentTemp.time}
+          measure={apiData?.hourly_units.temperature_2m}
+        />
+      </Grid>
+      <Grid
+        container
+        spacing={0}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-around"
+      >
+        <Grid item xs={10}>
+          <DayDiagram
+            times={getTimes(apiData?.hourly.time)}
+            measures={getMeasures(apiData?.hourly.temperature_2m)}
+          />
+          test
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Weather;
